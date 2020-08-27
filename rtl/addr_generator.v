d46149b7eea202cff46c7935c5302679b31f3a3e
Thiem Van Chu <thiem@artic.iir.titech.ac.jp>
2020-08-27 19:01:49 +0900

    generate sequential addresses instead of random addresses (for testing)

--- rtl/addr_generator.v
+++ rtl/addr_generator.v
@@ -9,28 +9,24 @@ module AddrGenerator #(
      output wire [ADDR_WIDTH-1 : 0] o_waddr,
      output wire [ADDR_WIDTH-1 : 0] o_raddr);
 
-    localparam RAND_SEED = 20200826;
+    reg  [ADDR_WIDTH-4 : 0] waddr;
+    reg  [ADDR_WIDTH-4 : 0] raddr;
 
-    wire [63:0] waddr;
-    wire [63:0] raddr;
+    assign o_waddr = {waddr, 3'b000};
+    assign o_raddr = {raddr, 3'b000};
 
-    assign o_waddr = {waddr[ADDR_WIDTH-4 : 0], 3'b000};
-    assign o_raddr = {raddr[ADDR_WIDTH-4 : 0], 3'b000};
-
-    Xorshift128plus #(
-                      .SEED(RAND_SEED))
-    rand_waddr (
-                .clk(clk),
-                .i_rst(i_rst),
-                .i_enable(i_wen),
-                .o_out(waddr));
-
-    Xorshift128plus #(
-                      .SEED(RAND_SEED))
-    rand_raddr (
-                .clk(clk),
-                .i_rst(i_rst),
-                .i_enable(i_ren),
-                .o_out(raddr));
+    always @(posedge clk) begin
+        if (i_rst) begin
+            waddr <= 0;
+            raddr <= 0;
+        end else begin
+            if (i_wen) begin
+                waddr <= waddr + 1;
+            end
+            if (i_ren) begin
+                raddr <= raddr + 1;
+            end
+        end
+    end
 
 endmodule
